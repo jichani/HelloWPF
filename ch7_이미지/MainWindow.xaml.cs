87e259cef49222aa623e69ad75bc27f7de530163
jichani <jichan7571@naver.com>
2023-10-19 11:48:43 +0900

    ch7_이미지 / 마우스오버 시 이미지 바뀌게 구현 및 텍스트 아래 정렬

---
+++ ch7_이미지/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ch7_이미지
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void img_MouseEnter(object sender, MouseEventArgs e)
+        {
+            img.Source = new BitmapImage(new Uri("https://cdn0.iconfinder.com/data/icons/phosphor-thin-vol-4/256/robot-thin-256.png"));
+        }
+    }
+}
