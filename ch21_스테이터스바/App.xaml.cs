b75d0f86fc6b5b74c1e3afd65e2e7a29e1514d5b
jichani <jichan7571@naver.com>
2023-10-20 10:44:52 +0900

    독패널과 잘어울리는 컨트롤 / 화면 하단에서 상태를 표시해준다.

---
+++ ch21_스테이터스바/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ch21_스테이터스바
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
